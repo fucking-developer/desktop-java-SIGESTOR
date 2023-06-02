4ffc8551cdd0278d58545da73048ae71f4a3d8dc
germancitom <gcruzm14158@gmail.com>
2023-06-02 15:58:49 -0600

    leiminacion de comentarios que estan por demas en dialogo ciclo

--- SIGESTOR/src/sigestor/gui/DialogoCiclo.java
+++ SIGESTOR/src/sigestor/gui/DialogoCiclo.java
@@ -825,7 +825,6 @@ public class DialogoCiclo extends JDialog {
 				for (int i = 0; i < tamaño; i++) {
 
 					for (Participante p : torneo.getListaParticipantes()) {
-						// si el participante p es igual al participante final del encuentro
 						if (p.getNumeroParticipante() == this
 								.obtenerParticipante(listaEncuentros.get(i).getIdParticipanteFinal())
 								.getNumeroParticipante()
@@ -876,7 +875,7 @@ public class DialogoCiclo extends JDialog {
 				}
 			} else {
 
-				// System.out.println("numero de partidas: " + numeroPartidas);
+			
 				System.out.println("tamaño= " + tamaño);
 				System.out.println("lista de encuentros: " + listaEncuentros);
 				for (int i = 0; i < tamaño; i++) {
